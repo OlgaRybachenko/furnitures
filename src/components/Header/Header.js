70db03c20bfb447f29f253f266ae67476106a1cb
OlgaRybachenko <rybachenkoolga@yandex.by>
2023-09-26 17:51:23 +0300

    new file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FaCartPlus } from "react-icons/fa6";
-import { Order } from './Order';
+import { Order } from '../Order';
+import './Header.css';
 
 const showOrders =(props) => {
   let summa = 0
--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,8 @@ export class Order extends Component {
   render() {
     return (
       <div className="item">
-        <img src={"./img/" + this.props.item.img} />
+        <img src={"./img/" + this.props.item.img} 
+        />
         <h2>{this.props.item.title}</h2>
         <b>{this.props.item.price} BY</b>
         <FaRegTrashCan className='delete' onClick={() => this.props.onDelete(this.props.item.id)} />
